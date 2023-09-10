36029ec09133f0df87b74595867dcc946ce5d1ed
sergxlove <sergxlove@mail.ru>
2023-09-10 16:44:11 +0600

    add class

--- action.h
+++ action.h
@@ -1,25 +1,38 @@
 #pragma once
 #ifndef _ACTION_H_
 #define _ACTION_H_
+#include <list>
 class action
 {
 public:
-	virtual int to_bin() = 0;
-	virtual int to_oct() = 0;
-	virtual int to_hex() = 0;
+	virtual std::list<int> to_bin() = 0;
+	virtual std::list<int> to_oct() = 0;
+	virtual std::list<int> to_hex() = 0;
 private:
 };
 class action_over_dec : public action
 {
-	int to_bin() override;
-	int to_oct() override;
-	int to_hex() override;
+public:
+	action_over_dec();
+	~action_over_dec();
+	std::list<int> to_bin() override;
+	std::list<int> to_oct() override;
+	std::list<int> to_hex() override;
+private:
+	std::list<int> result;
+	int value = 0;
 };
 class action_to_dec : public action
 {
-	int to_bin() override;
-	int to_oct() override;
-	int to_hex() override;
+public:
+	action_to_dec();
+	~action_to_dec();
+	std::list<int> to_bin() override;
+	std::list<int> to_oct() override;
+	std::list<int> to_hex() override;
+private:
+	std::list<int> result;
+	int value = 0;
 };
 #endif
 
